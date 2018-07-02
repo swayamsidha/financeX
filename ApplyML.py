d00066d5738953a0de24cf6f6caf2a22b728230f
swayam <chandan20july1997@gmail.com>
2018-07-02 14:17:30 +0530

    add k-mean clustering

--- ApplyML.py
+++ ApplyML.py
@@ -54,4 +54,30 @@ def extract_features(ticker):
 '''
 we will use k nearest neighbour method
 '''
+def do_ml(ticker):
+    X, y, df = extract_featuresets(ticker)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25)
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
+    #take x data and fit it to y data
+    confidence = clf.score(X_test, y_test)
+    #predict the output
+    predictions = clf.predict(X_test)
+    print('accuracy:',confidence)
+    return confidence
+    print('prediction class count:',Counter(predictions))
+with open("sp500tickers.pickle","rb") as f:
+    tickers = pickle.load(f)
+    
 
+accuracies = []
+for count,ticker in enumerate(tickers[:10]):
+
+    if count%10==0:
+        print(count)
+
+    accuracy = do_ml(ticker)
+    accuracies.append(accuracy)
+    print(accuracy)
+print("\n mean accuracy is:")
+print(mean(accuracies))
